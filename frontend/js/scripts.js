5dd07d64113a24b77e7881d9a897a7ae536b424d
KarasDominik <dominik.karas99@gmail.com>
2024-06-20 21:13:38 +0200

    UI improvements

--- frontend/js/scripts.js
+++ frontend/js/scripts.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
 function updateTable() {
     fetchTasks().then(response => {
         const tasks = response.tasks;
-        if (tasks === 0) {
+        if (tasks.length === 0) {
             document.getElementById("output").innerHTML = '<p>No tasks yet! Add one above!</p>';
         } else {
             document.getElementById("output").innerHTML = generateTable(tasks);
@@ -26,7 +26,12 @@ function generateTable(tasks) {
         table += `<tr id="task">`;
         table += `<td><input type="checkbox" ${task.done ? 'checked' : ''} onclick="updateTask('${task.taskId}')"></td>`;
         table += `<td id="${task.done ? 'done-task' : ''}">${task.content}</td>`;
-        table += `<td><button id="trash-bin" class="glyphicon glyphicon-trash" onclick="deleteTask('${task.taskId}')">Usuń</button></td>`;
+        table += `<td>
+                        <svg id="trash-bin" onclick="deleteTask('${task.taskId}')" xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+                            <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+                         </svg>
+                  </td>`;
         table += `</tr>`;
     });
 
