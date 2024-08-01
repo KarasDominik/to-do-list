5be3c12036492b8cb04bbf42854eb4f41463a271
KarasDominik <dominik.karas99@gmail.com>
2024-08-01 22:08:42 +0200

    Change trash icon from svg to bootstrap

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -26,12 +26,7 @@ function generateTable(tasks) {
         table += `<tr id="task">`;
         table += `<td><input type="checkbox" ${task.done ? 'checked' : ''} onclick="updateTask('${task.taskId}')"></td>`;
         table += `<td id="${task.done ? 'done-task' : ''}">${task.content}</td>`;
-        table += `<td>
-                        <svg id="trash-bin" onclick="deleteTask('${task.taskId}')" xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-                            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
-                            <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
-                         </svg>
-                  </td>`;
+        table += `<td><i id="trash-bin" class="bi bi-trash" onclick="deleteTask('${task.taskId}')"></i></td>`;
         table += `</tr>`;
     });
 
